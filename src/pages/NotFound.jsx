dbe6ad012b0c5279037707193ed2d63c43f01562
Cristian Alvarado <alvarado@upre.gob.bo>
2024-03-06 17:16:40 -0400

    'actual'

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 
 export const NotFound = () => {
   return (
-    <div>Página no disponible</div>
+    <div>
+      <h1 style={{letterSpacing:'1.6rem',fontWeight:'bolder',color:'powderblue',fontSize:'140px',borderBottom:'2px solid powderblue'}}>404</h1>
+      <p style={{letterSpacing:'0.15rem',fontSize:'22px'}}>Oops! esta ruta es inaccesible, por favor retorne a la <a href="javascript:history.back()">pagina anterior</a> o al <a href="/">inicio</a>.</p>
+    </div>
   )
 }
